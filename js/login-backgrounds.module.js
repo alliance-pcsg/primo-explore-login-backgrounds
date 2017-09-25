b180a9a68b7e00757c6b54e05fc858bfd5a2d040
budak <thatbudakguy@gmail.com>
2017-09-25 15:05:27 -0700

    (#2) Add tests

--- js/login-backgrounds.module.js
+++ js/login-backgrounds.module.js
@@ -1,26 +1,24 @@
 angular
   .module('loginBackgrounds', [])
   .component('prmStandAloneLoginAfter', {
-    controller: ['loginBackgroundsService', function (loginBackgroundsService) {
-      this.$onInit = function () { loginBackgroundsService.setBackground() }
-      this.$onDestroy = function () { loginBackgroundsService.clearBackground() }
+    controller: ['loginBackgrounds', function (loginBackgrounds) {
+      this.$onInit = function () { loginBackgrounds.setBackground() }
+      this.$onDestroy = function () { loginBackgrounds.clearBackground() }
     }]
   })
-  .factory('loginBackgroundsService', ['$document', '$stateParams', 'loginImages',
+  .factory('loginBackgrounds', ['$document', '$stateParams', 'loginImages',
     function ($document, $stateParams, loginImages) {
       return {
         get bodyElement() {
           return angular.element($document.find('body')[0])
         },
-        chooseBackground: function() {
-          return 'custom/' + $stateParams.vid + '/img/login/' + loginImages[Math.floor(Math.random() * loginImages.length)]
-        },
         setBackground: function () {
-          let background = this.chooseBackground()
-          this.bodyElement.css('background', `url("${background}")`)
+          var background = 'custom/' + $stateParams.vid + '/img/login/' + loginImages[Math.floor(Math.random() * loginImages.length)]
+          this.bodyElement.css('background', 'url("' + background + '")')
           this.bodyElement.css('background-position', 'center')
           this.bodyElement.css('background-size', 'cover')
           this.bodyElement.css('background-repeat', 'no-repeat')
+          return background
         },
         clearBackground: function () {
           this.bodyElement.css('background', '')
